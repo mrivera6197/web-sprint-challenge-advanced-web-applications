cab9c26527da9dfc5ecf422302d5e1fd0a66993e
Mali Rivera <mali@Malis-MacBook-Pro.local>
2020-12-18 11:44:46 -0500

    fetchColors working

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -4,10 +4,26 @@ import axios from "axios";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
+import { fetchColors } from '../api/fetchColors'
+
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  const getData = () => {
+    fetchColors()
+    .then(res => {
+      setColorList(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <>
